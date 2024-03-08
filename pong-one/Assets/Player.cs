1de1686407f6cf0aee5417d15578fc306e4b9079
Muncherxyz <19kortlanblocker@gmail.com>
2024-03-08 09:24:01 -0600

    [added Keys]

--- pong-one/Assets/Player.cs
+++ pong-one/Assets/Player.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class Player : MonoBehaviour
 {
 
-
-    public Rigidbody2D rb;
+    public KeyCode upKey;
+    public KeyCode downKey;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(1000f, 1000f);   
+        
     }
 
     // Update is called once per frame
--- pong-one/Assets/Scripts/Paddle.cs
+++ pong-one/Assets/Scripts/Paddle.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class Paddle : MonoBehaviour
 {
-
-
-    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(1000f, 1000f);   
+        
     }
 
     // Update is called once per frame
