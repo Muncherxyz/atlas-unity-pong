cf1e0567a0490dcff2b0af40298736451a45e6c8
Muncherxyz <19kortlanblocker@gmail.com>
2024-03-13 15:08:51 -0500

    [Added SFX based on collision, MainMenu functionality, Score increase on ball collision w/ goal]

--- pong-one/Assets/Scripts/Ball.cs
+++ pong-one/Assets/Scripts/Ball.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Ball : MonoBehaviour
 {
@@ -8,7 +9,14 @@ public class Ball : MonoBehaviour
     public float ballSpeed;
     public Rigidbody2D rb;
     public Vector2 direction;
+
+    public TMP_Text LeftScore;
+    public TMP_Text RightScore;
+
+    public AudioSource audioPlayer;
+    public AudioSource goalPlayer;
     
+    public AudioSource bonkSound;
 
     // Start is called before the first frame update
     void Start()
@@ -35,17 +43,38 @@ public class Ball : MonoBehaviour
         transform.localPosition = new Vector2(0, 0);
         Invoke("StartBall", 2f);
     }
-    
+
 
     void OnTriggerEnter2D(Collider2D Collision) 
     {
         if(Collision.gameObject.CompareTag("Paddle"))
         {
+            bonkSound.Play();
             rb.velocity *= -1;
         }
         if(Collision.gameObject.CompareTag("Goal"))
         {
-            ResetBall();
+            if(transform.localPosition.x > 0)
+            {
+                ResetBall();
+                goalPlayer.Play();
+                LeftScore.text = (int.Parse(LeftScore.text) + 1).ToString();
+            }
+            else if(transform.localPosition.x < 0)
+            {
+                ResetBall();
+                goalPlayer.Play();
+                RightScore.text = (int.Parse(RightScore.text) + 1).ToString();
+            }
         }
     }
+
+    void OnCollisionEnter2D(Collision2D Collision)
+    {
+        if(Collision.gameObject.tag == "Wall")
+        {
+            audioPlayer.Play();
+        }
+    }
+
 }
